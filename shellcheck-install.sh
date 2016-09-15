07dccd1612bcf5251b356fd64b373c72ed36a0a0
Cristian Martinez <martinec@users.noreply.github.com>
2016-09-15 18:12:15 +0200

    [ci minor fix] Update shellcheck download link

--- shellcheck-install.sh
+++ shellcheck-install.sh
@@ -8,7 +8,7 @@ main() {
     brew update
     brew install bash shellcheck
   else
-    local filename="shellcheck_0.3.7-5_amd64.deb"
+    local filename="shellcheck_0.4.4-2_amd64.deb"
     wget "http://ftp.debian.org/debian/pool/main/s/shellcheck/$filename"
     sudo dpkg -i "$filename"
   fi
